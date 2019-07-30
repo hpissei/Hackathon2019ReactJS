6587dc704316d98e10fe24153ee54af04f71b196
Heetesh Panghanti <Heetesh.Panghanti@harbingergroup.com>
2019-07-30 06:25:17 -0700

    UI changes done for both pages

--- src/EvaluatorHeader/EvaluatorHeader.js
+++ src/EvaluatorHeader/EvaluatorHeader.js
@@ -7,21 +7,15 @@ const EvaluatorHeader = () => {
             <div className="bluestrip">
                 <div className="candidate-title-wrapper clearfix">
                     <h1 className="head-title align-left">My Evaluators</h1>
-                    <div className="align-right">
-                        <div className="selectout">
-                            <label >Blank Text</label>
-                            <select id="ddlLanguage" name="ddlLanguage" defaultValue="en-US"><option value="en-US">English</option>
-                            </select>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div className="bredcumbs" id="div_breadcrumbs" role="navigation" aria-label="breadcrumb">
                 <ul className="clearfix">
-                    <li>Instructions</li>
-                    <li>Verify My Information</li>
-                    <li id="lnkOptionalResearch" className="display-none" >Optional Research</li>
-                    <li aria-current="page"><b>My Evaluators</b></li>
+                    <li className="breadcrumb-item">Instructions</li>
+                    <li className="breadcrumb-item"> Verify My Information</li>
+                    <li id="lnkOptionalResearch" className="breadcrumb-item" >Optional Research</li>
+                    <li aria-current="page" className="breadcrumb-item"><b>My Evaluators</b></li>
                 </ul>
             </div>
         </div>
--- src/Evaluators/Verfication/Verification.js
+++ src/Evaluators/Verfication/Verification.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import EvaluatorHeader from '../../EvaluatorHeader/EvaluatorHeader';
 import EvaluatorInformationText from '../../EvaluatorInformationText/EvaluatorInformationText';
+import ContactDetails from './ContactDetails/ContactDetails';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Verification = () => {
 
     return (
         <div>
             <EvaluatorHeader> </EvaluatorHeader>
-            <EvaluatorInformationText />
-
+            {/* <EvaluatorInformationText /> */}
+            <ContactDetails></ContactDetails>
         </div>
     )
 };
